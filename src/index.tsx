09c422586a8281f6d46245081371dccd7ee87b60
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-09-17 21:26:26 +0900

    fix:productService fixing

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import App from "./services/App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
