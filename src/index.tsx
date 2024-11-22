af2f62a55200f09caca27955cb2d480506061b01
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-22 19:37:39 +0900

    feat: develop ScoketIo client

--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,11 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/components/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import "../src/css/index.css";
 import App from "./app/App";
 import ContextProvider from "./app/context/Context.Provider";
+import { SocketProvider } from "./app/context/SocketContext";
 import "./utils/i18n";
+import "../src/css/index.css";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
@@ -20,12 +21,14 @@ root.render(
    <React.StrictMode>
       <Provider store={store}>
          <ContextProvider>
-            <ThemeProvider theme={theme}>
-               <CssBaseline />
-               <Router>
-                  <App />
-               </Router>
-            </ThemeProvider>
+            <SocketProvider>
+               <ThemeProvider theme={theme}>
+                  <CssBaseline />
+                  <Router>
+                     <App />
+                  </Router>
+               </ThemeProvider>
+            </SocketProvider>
          </ContextProvider>
       </Provider>
    </React.StrictMode>,
