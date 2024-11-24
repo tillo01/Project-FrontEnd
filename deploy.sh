64569775940a35d615766ec9f1d9b8fff8b5a262
Tillochik01 <xuribayevzakaryo@gmail.com>
2024-11-24 14:14:00 +0900

    feat: develop projects

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #PRODUCTION
-# git reset --hard
-# git checkout master
-# git pull origin master
+git reset --hard
+git checkout master
+git pull origin master
 
 npm i yarn -g
 yarn global add serve 
